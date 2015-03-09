697c829a91cf16331926aa0c4db0712c2ec00dd3
shengsheng <qianzhangsheng@yeah.net>
2015-03-09 11:01:43 +0800

    commit

--- demo/html5/canvas/index.js
+++ demo/html5/canvas/index.js
@@ -2,17 +2,9 @@ var canvas = document.getElementById('myCanvas');
 
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-    ctx.beginPath();                // 开始路径绘制
-    ctx.moveTo(20, 20);             // 设置路径起点，坐标为(20,20)
-    ctx.lineTo(200, 20);            // 绘制一条到(200,20)的直线
-    ctx.lineWidth = 1.0;            // 设置线宽
-    ctx.strokeStyle = '#cc0000';    // 设置线的颜色
-    ctx.stroke();                   // 进行线的着色，这时整条线才变的可见
-
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(50, 50, 200, 100);
-
-    ctx.strokeRect(80, 80, 200, 100);
-
-    ctx.clearRect(120, 60, 50, 50);
+    var myGradient = ctx.createLinearGradient(0, 0, 0, 160);
+    myGradient.addColorStop(0, '#bababa');
+    myGradient.addColorStop(1, '#636363');
+    ctx.fillStyle = myGradient;
+    ctx.fillRect(10, 10, 200, 100);
 }
