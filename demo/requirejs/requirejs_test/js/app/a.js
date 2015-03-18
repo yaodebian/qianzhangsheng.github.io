52e9d2cebc57e569ba3c0a5b18e8efb72f2c2127
qianzhangsheng <qianzhangsheng@yeah.net>
2015-03-18 09:20:08 +0800

    commit

--- demo/requirejs/requirejs_test/js/app/a.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by qianzhangsheng on 14-10-31.
- */
-define(["b"], function (b) {
-    return b;
-});
--- demo/requirejs/requirejs_test/js/app/b.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by qianzhangsheng on 14-10-31.
- */
-define(function () {
-    return 1;
-});
